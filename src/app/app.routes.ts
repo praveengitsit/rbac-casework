69c5600d098c3bcd3c705720c9c60a40d5a94c66
praveenbhusal <praveenbhusal97@gmail.com>
2025-05-27 19:34:39 +0545

    feat(architecture): New dependencies and route configurations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/components/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./features/home/home.component').then((m) => m.HomeComponent),
+    children: [
+      {
+        path: 'roles',
+        loadComponent: () =>
+          import('./features/role-management/role-management.component').then(
+            (m) => m.RoleManagementComponent,
+          ),
+      },
+      {
+        path: 'users',
+        loadComponent: () =>
+          import('./features/user-management/user-management.component').then(
+            (m) => m.UserManagementComponent,
+          ),
+      },
+    ],
+  },
+];
