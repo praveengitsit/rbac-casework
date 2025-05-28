e1657778606a24d7f819cb0adc9afeee653094f3
praveenbhusal <praveenbhusal97@gmail.com>
2025-05-28 15:33:59 +0545

    feat(role): Add role view

--- src/app/features/role-management/components/role-management.component.ts
+++ src/app/features/role-management/components/role-management.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { Role } from '../models/role';
+import { RoleService } from '../services/role.service';
+import { ChipModule } from 'primeng/chip';
 
 @Component({
   selector: 'app-role-management',
-  imports: [TableModule],
+  imports: [TableModule, ChipModule],
   templateUrl: './role-management.component.html',
   styleUrl: './role-management.component.scss',
 })
-export class RoleManagementComponent {
+export class RoleManagementComponent implements OnInit {
   public roles: Role[] = [];
+
+  constructor(private roleService: RoleService) {}
+
+  ngOnInit(): void {
+    this.getRoleList();
+  }
+
+  public getRoleList(): void {
+    this.roleService.getRoleList().subscribe((roles) => {
+      this.roles = roles;
+    });
+  }
 }
--- src/app/features/role-management/services/role.service.ts
+++ src/app/features/role-management/services/role.service.ts
@@ -1,12 +1,33 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { Role } from '../models/role';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RoleService {
   private readonly baseURL = environment.apiUrl;
-  private readonly getRolesApiEndpoint = `${this.baseURL}/protected/roles`;
+  private readonly rolesApiEndpoint = `${this.baseURL}/api/roles`;
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    //
+  }
+
+  getRoleList(): Observable<Role[]> {
+    return this.http.get<Role[]>(this.rolesApiEndpoint);
+  }
+
+  addRole(role: Role): Observable<Role> {
+    return this.http.post<Role>(this.rolesApiEndpoint, role);
+  }
+
+  updateRole(role: Role): Observable<Role> {
+    return this.http.put<Role>(this.rolesApiEndpoint, role);
+  }
+
+  deleteRole(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.rolesApiEndpoint}/${id}`);
+  }
 }
