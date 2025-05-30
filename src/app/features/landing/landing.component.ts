03518e9848b392caf88ef60433a3d50c54282119
praveenbhusal <praveenbhusal97@gmail.com>
2025-05-30 16:39:02 +0545

    feat(landing): Add landing page for logged in users

---
+++ src/app/features/landing/landing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-landing',
+  imports: [MatIcon],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.scss',
+})
+export class LandingComponent {}
